eefb1c236e11519a41102ee56842ea7e39a13894
Emil Backlund <emilbacklund@outlook.com>
2020-05-07 17:30:04 +0200

    first commit

---
+++ driver.class.php
@@ -0,0 +1,11 @@
+<?php
+// Bygga en väg som består av 12 delar 
+// 4 höger
+// 4 raka
+// 4 vänster
+
+// Driver väljer väg för att testa sig fram vilken som är rätt. 
+
+// <<<< Behöver göras >>>>
+// Spara till fil varje rätt resultat. 
+// Skapa en funktion 
